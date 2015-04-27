e58c3168ab4c6ac844cb22dbd84b486b079b2183
Dennis Hoer <dennis.hoer@pearson.com>
2015-04-26 23:55:42 -0700

    Fix setup

--- test/fixtures/cookbooks/macosx_accessibility_test/recipes/setup.rb
+++ test/fixtures/cookbooks/macosx_accessibility_test/recipes/setup.rb
@@ -1,17 +1,7 @@
 return unless platform_family?('mac_os_x')
 
-# https://support.apple.com/en-us/HT201710
-execute 'activate remote desktop sharing for vagrant' do
-  command <<EOF
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
- -activate -configure -access -on -users vagrant -privs -all -restart -agent -menu
-EOF
-end
-
 execute 'switch to login window' do
-  command <<EOF
-sudo "/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession" -suspend
-EOF
+  command 'sudo "/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession" -suspend'
 end
 
 execute 'login to gui' do
@@ -26,12 +16,11 @@ osascript -e '
     keystroke tab
     keystroke return
     keystroke return
-  end tell
-'
+  end tell'
 EOF
 end
 
 execute 'wait for login to build remote desktop db' do
-  command 'sleep 30'
+  command 'sleep 15'
   not_if { ::File.exist?('/Library/Application Support/com.apple.TCC/TCC.db') }
 end
