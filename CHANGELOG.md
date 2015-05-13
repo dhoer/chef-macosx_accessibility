d26019f6a8b286159bc2dfe1d68dc592b9c743da
Dennis Hoer <dennis.hoer@pearson.com>
2015-05-13 13:48:31 -0700

    Remove Chef 12 source_url and issues_url from metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.1
+
+- Remove Chef 12 source_url and issues_url from metadata
+
 ## 1.0.0
 
 - Enable use_inline_resources in the provider
--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,6 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description "Configures Mac OS X Accessibility using Jacob Salmela's tccutil"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/dhoer/chef-macosx_accessibility'
-issues_url 'https://github.com/dhoer/chef-macosx_accessibility/issues'
-version '1.0.0'
+version '1.0.1'
 
 supports 'mac_os_x', '>= 10.9'
